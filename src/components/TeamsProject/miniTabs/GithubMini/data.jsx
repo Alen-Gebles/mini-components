c44e14de44a8a173495d06c9fb51611d1ee8c84f
Zero <alengebles@gmail.com>
2024-12-12 12:25:54 +0100

    Added Teams MIni

--- src/components/TeamsProject/miniTabs/GithubMini/data.jsx
+++ src/components/TeamsProject/miniTabs/GithubMini/data.jsx
@@ -3,6 +3,44 @@ import './githubMini.css'
 
 export default function GithubMini() {
   return (
-    <div className='miniTabBox'>GithubMini</div>
+    <div className='miniTabBox miniGitTabBox'>
+
+      <div className='gitMiniRow miniRow'>
+        <div className='gitMiniRow_left'>
+          <svg viewBox="0 0 16 16" width="auto" height="auto"><path d="M1.5 3.25a2.25 2.25 0 1 1 3 2.122v5.256a2.251 2.251 0 1 1-1.5 0V5.372A2.25 2.25 0 0 1 1.5 3.25Zm5.677-.177L9.573.677A.25.25 0 0 1 10 .854V2.5h1A2.5 2.5 0 0 1 13.5 5v5.628a2.251 2.251 0 1 1-1.5 0V5a1 1 0 0 0-1-1h-1v1.646a.25.25 0 0 1-.427.177L7.177 3.427a.25.25 0 0 1 0-.354ZM3.75 2.5a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5Zm0 9.5a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5Zm8.25.75a.75.75 0 1 0 1.5 0 .75.75 0 0 0-1.5 0Z"></path></svg>
+          <p>#793</p>
+        </div>
+
+        <div className='gitMiniRow_right'>
+          <p>replaced-api-url</p>
+          <svg width="auto" height="auto" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M10 8V7C10 6.05719 10 5.58579 10.2929 5.29289C10.5858 5 11.0572 5 12 5H17C17.9428 5 18.4142 5 18.7071 5.29289C19 5.58579 19 6.05719 19 7V12C19 12.9428 19 13.4142 18.7071 13.7071C18.4142 14 17.9428 14 17 14H16M7 19H12C12.9428 19 13.4142 19 13.7071 18.7071C14 18.4142 14 17.9428 14 17V12C14 11.0572 14 10.5858 13.7071 10.2929C13.4142 10 12.9428 10 12 10H7C6.05719 10 5.58579 10 5.29289 10.2929C5 10.5858 5 11.0572 5 12V17C5 17.9428 5 18.4142 5.29289 18.7071C5.58579 19 6.05719 19 7 19Z" stroke="#464455" stroke-linecap="round" stroke-linejoin="round"></path></g></svg>
+        </div>
+      </div>
+
+      <div className='gitMiniRow miniRow'>
+        <div className='gitMiniRow_left'>
+          <svg viewBox="0 0 16 16" width="auto" height="auto"><path d="M1.5 3.25a2.25 2.25 0 1 1 3 2.122v5.256a2.251 2.251 0 1 1-1.5 0V5.372A2.25 2.25 0 0 1 1.5 3.25Zm5.677-.177L9.573.677A.25.25 0 0 1 10 .854V2.5h1A2.5 2.5 0 0 1 13.5 5v5.628a2.251 2.251 0 1 1-1.5 0V5a1 1 0 0 0-1-1h-1v1.646a.25.25 0 0 1-.427.177L7.177 3.427a.25.25 0 0 1 0-.354ZM3.75 2.5a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5Zm0 9.5a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5Zm8.25.75a.75.75 0 1 0 1.5 0 .75.75 0 0 0-1.5 0Z"></path></svg>
+          <p>#186</p>
+        </div>
+
+        <div className='gitMiniRow_right'>
+          <p>updated-custom-hooks</p>
+          <svg width="auto" height="auto" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M10 8V7C10 6.05719 10 5.58579 10.2929 5.29289C10.5858 5 11.0572 5 12 5H17C17.9428 5 18.4142 5 18.7071 5.29289C19 5.58579 19 6.05719 19 7V12C19 12.9428 19 13.4142 18.7071 13.7071C18.4142 14 17.9428 14 17 14H16M7 19H12C12.9428 19 13.4142 19 13.7071 18.7071C14 18.4142 14 17.9428 14 17V12C14 11.0572 14 10.5858 13.7071 10.2929C13.4142 10 12.9428 10 12 10H7C6.05719 10 5.58579 10 5.29289 10.2929C5 10.5858 5 11.0572 5 12V17C5 17.9428 5 18.4142 5.29289 18.7071C5.58579 19 6.05719 19 7 19Z" stroke="#464455" stroke-linecap="round" stroke-linejoin="round"></path></g></svg>
+        </div>
+      </div>
+
+      <div className='gitMiniRow miniRow'>
+        <div className='gitMiniRow_left'>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="auto" height="auto"><path d="M3.75 4.5a1.25 1.25 0 1 0 0-2.5 1.25 1.25 0 0 0 0 2.5ZM3 7.75a.75.75 0 0 1 1.5 0v2.878a2.251 2.251 0 1 1-1.5 0Zm.75 5.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm5-7.75a1.25 1.25 0 1 1-2.5 0 1.25 1.25 0 0 1 2.5 0Zm5.75 2.5a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-1.5 0a.75.75 0 1 0-1.5 0 .75.75 0 0 0 1.5 0Z"></path></svg>
+          <p>#77</p>
+        </div>
+
+        <div className='gitMiniRow_right'>
+          <p>updated-dependencies</p>
+          <svg width="auto" height="auto" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M10 8V7C10 6.05719 10 5.58579 10.2929 5.29289C10.5858 5 11.0572 5 12 5H17C17.9428 5 18.4142 5 18.7071 5.29289C19 5.58579 19 6.05719 19 7V12C19 12.9428 19 13.4142 18.7071 13.7071C18.4142 14 17.9428 14 17 14H16M7 19H12C12.9428 19 13.4142 19 13.7071 18.7071C14 18.4142 14 17.9428 14 17V12C14 11.0572 14 10.5858 13.7071 10.2929C13.4142 10 12.9428 10 12 10H7C6.05719 10 5.58579 10 5.29289 10.2929C5 10.5858 5 11.0572 5 12V17C5 17.9428 5 18.4142 5.29289 18.7071C5.58579 19 6.05719 19 7 19Z" stroke="#464455" stroke-linecap="round" stroke-linejoin="round"></path></g></svg>
+        </div>
+      </div>
+
+    </div>
   )
 }
--- src/components/TeamsProject/miniTabs/TeamsMini/data.jsx
+++ src/components/TeamsProject/miniTabs/TeamsMini/data.jsx
@@ -3,6 +3,52 @@ import './teamsMini.css'
 
 export default function TeamsMini() {
   return (
-    <div className='miniTabBox'>TeamsMini</div>
+    <div className='miniTabBox miniTeamTabBox'>
+
+      <div className='miniRow'>
+        <div className='teamMiniRow_left'>
+          <p>Evelyn Harper</p>
+          <p>Pecs, Hungary</p>
+        </div>
+
+        <div className='teamMiniRow_right'>
+          <p>08:30 AM</p>
+        </div>
+      </div>
+
+      <div className='miniRow'>
+        <div className='teamMiniRow_left'>
+          <p>Clara Rossi</p>
+          <p>Matera, Italy</p>
+        </div>
+
+        <div className='teamMiniRow_right'>
+        <p>5:30 PM</p>
+        </div>
+      </div>
+
+      <div className='miniRow'>
+        <div className='teamMiniRow_left'>
+          <p>Noah Fischer</p>
+          <p>Tbilisi, Georgia</p>
+        </div>
+
+        <div className='teamMiniRow_right'>
+        <p>2:45 PM</p>
+        </div>
+      </div>
+
+      <div className='miniRow'>
+        <div className='teamMiniRow_left'>
+          <p>Lucas Schmidt</p>
+          <p>Vilnius, Lithuania</p>
+        </div>
+
+        <div className='teamMiniRow_right'>
+        <p>01:00 PM</p>
+        </div>
+      </div>
+
+    </div>
   )
 }
